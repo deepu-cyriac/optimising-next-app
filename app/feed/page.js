b8c000e3ce0682821df9a345ac8a0a88bb3e7866
Deepu Cyriac <deepucvk123@gmail.com>
2024-08-07 19:20:44 +0530

    Added static and dynamic metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,17 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+//dynamic metadata
+//reserved function
+//executed when page is rendered
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse the latest ${numberOfPosts} posts`,
+    description: "Browse all our posts",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
-import Header from '@/components/header';
-import './globals.css';
+import Header from "@/components/header";
+import "./globals.css";
 
+//the metadata set in layout will be used in every page which uses the layout. the metadata will be merged with page metadata if exists
 export const metadata = {
-  title: 'NextPosts',
-  description: 'Browse and share amazing posts.',
+  title: "NextPosts",
+  description: "Browse and share amazing posts.",
 };
 
 export default function RootLayout({ children }) {
