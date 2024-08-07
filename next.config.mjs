95fb26e1761b72a67845887a9206fc14adff841e
Deepu Cyriac <deepucvk123@gmail.com>
2024-08-07 07:18:12 +0530

    Added config for allowing images from external sites and some css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "res.cloudinary.com" }], //to allow next.js to fetch images from external site
+  },
+};
 
 export default nextConfig;
